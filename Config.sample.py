9a24847f8dcd7a95a21882408f5e15df1225c4ff
Michele Pinto <michele.pinto@qa.com>
2026-09-16 22:59:51 +0200

    no message

--- Config.sample.py
+++ Config.sample.py
@@ -21,8 +21,8 @@ excluded_keywords = ""
 # Vinted queries for research
 # "page", "per_page" and "order" you may not edit them
 # "search_text" is the free search field, this field may be empty if you wish to search for the entire brand.
-# "catalog_ids" is the category in which to eventually search, if the field is empty it will search in all categories. Vinted assigns a numeric ID to each category, e.g. 2996 is the ID for e-Book Reader
-# "brand_ids" if you want to search by brand. Vinted assigns a numeric ID to each brand, e.g. 417 is the ID for Louis Vuitton
+# "attribute_ids[catalog]" is the category in which to eventually search, if the field is empty it will search in all categories. Vinted assigns a numeric ID to each category, e.g. 2996 is the ID for e-Book Reader
+# "attribute_ids[brand]" if you want to search by brand. Vinted assigns a numeric ID to each brand, e.g. 417 is the ID for Louis Vuitton
 # "order" you can change it to relevance, newest_first, price_high_to_low, price_low_to_high
 
 queries = [
