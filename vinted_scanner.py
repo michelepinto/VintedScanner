e01641638caa69e96435e291a9a7337626bbd9bb
Michele Pinto <michele.pinto@qa.com>
2026-09-16 23:01:19 +0200

    no message

--- vinted_scanner.py
+++ vinted_scanner.py
@@ -222,10 +222,13 @@ def main():
                 params=params
             ).prepare()
 
-            logger.info(f"Vinted URL triggered: {prepared_request.url}")
+            logger.info(f"Vinted URL triggered1: {prepared_request.url}")
 
             from urllib.parse import unquote
             logger.info(f"Vinted URL triggered2: {email.utils.unquote(prepared_request.url)}")
+
+            logger.info("Vinted URL triggered3: %s", prepared_request.url)
+            logger.info("Vinted URL repr4: %r", prepared_request.url)
 
             response = requests.get(
                 url,
