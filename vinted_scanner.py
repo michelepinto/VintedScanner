5606943bd7b88bbefe07cc639bb95bc5fc53a47d
Michele Pinto <michele.pinto@qa.com>
2026-09-17 10:43:41 +0200

    no message

--- vinted_scanner.py
+++ vinted_scanner.py
@@ -35,19 +35,23 @@ timeoutconnection = 30
 list_analyzed_items = set()
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:128.0) Gecko/20100101 Firefox/128.0",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
     "Accept-Language": "it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3",
-    "DNT": "1",
-    "Connection": "keep-alive",
+    #"DNT": "1",
+    #"Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
+    "Priority": "u=0, i",
+    "Sec-GPC": "1",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "macOS",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-GPC": "1",
-    "Priority": "u=0, i",
-    "Pragma": "no-cache",
-    "Cache-Control": "no-cache",
+    "Sec-Fetch-Site": "none",
+    "Ssec-fetch-user": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    #"Pragma": "no-cache",
+    #"Cache-Control": "no-cache",
 }
 
 # Load previously analyzed item hashes to avoid duplicates
