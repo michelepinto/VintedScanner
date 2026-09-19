f71e5be4e52bcf540470573ffef4d0e09f5cfdb4
Michele Pinto <17676813+michelepinto@users.noreply.github.com>
2026-09-19 09:19:42 +0200

    Update vinted_scanner.py

--- vinted_scanner.py
+++ vinted_scanner.py
@@ -161,8 +161,8 @@ def send_telegram_message(item_title, item_price, item_url, item_image, favourit
 
     safe_title = escape(str(item_title))
 
-    if len(safe_title) > 33:
-        safe_title = safe_title[:30] + "…"
+    if len(safe_title) > 34:
+        safe_title = safe_title[:31] + "…"
     
     safe_url = escape(str(item_url), quote=True)
     
