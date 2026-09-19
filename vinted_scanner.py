b3bca46df37b09ff1fb74f6daa3f6377a440b83a
Michele Pinto <17676813+michelepinto@users.noreply.github.com>
2026-09-19 09:12:01 +0200

    Shorten fee description in item price format

--- vinted_scanner.py
+++ vinted_scanner.py
@@ -306,7 +306,7 @@ def main():
                 item_currency = '€'
                 item_price = (
                     f"{item_amount} {item_currency} "
-                    f"({item_total_amount} {item_currency} With Fee)"
+                    f"({item_total_amount} {item_currency} w/Fee)"
                     if item_amount != "N/D" and item_total_amount != "N/D"
                     else "N/D"
                 )
